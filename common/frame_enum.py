d3d617923ec608b5e80d1afe8398d0ad43aed268
Nicky van Steensel van der Aa <nvsteenselvanderaa@brocacef.nl>
2019-04-18 17:29:02 +0200

    added dynamic generation of the packet types enum, en made the functions more suitable for further generation of more files

---
+++ common/frame_enum.py
@@ -0,0 +1,14 @@
+
+from common.common import AutoNumber
+
+class FrameType(AutoNumber):
+    NONE = ()
+
+    BUTTON_STATE = ()
+    ACTIVITY_LED_STATE = ()
+    DISTANCE = ()
+    DISPLAY_FILLED_RECTANGLE = ()
+
+    EXTERNAL = ()
+    ALL = ()
+    COUNT = ()
