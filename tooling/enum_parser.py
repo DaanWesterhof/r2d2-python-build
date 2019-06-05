797a17d8074792ac7e8a5005437a011f039a3224
Sebastiaan Saarloos <sebastiaan.saarloos@gmail.com>
2019-06-05 22:10:34 +0200

    Added documentation to enum_parser.py and enum_writer.py

--- tooling/enum_parser.py
+++ tooling/enum_parser.py
@@ -27,40 +27,47 @@ class CxxEnum:
 
 
 def get_enum_strings(file_contents: str) -> list:
-    # Remove comments
-    print(file_contents)
+    """Scrapes all the enum strings from a given string"""
+    "Remove all comments from the file."
     file_contents = re.sub("//.*\n", "", file_contents)
-    #remove redundant spaces
+    "Remove redundant spaces"
     file_contents = re.sub("\s+", " ", file_contents)
+    "enum class regular expression"
     pattern = re.compile("enum class\\s*\\w+\\s*\\:\\s*\\w+\\s*\\{[^\\{\\}]+\\};")
-    # return [item.replace("\n", "") for item in pattern.findall(file_contents)]
-    for item in pattern.findall(file_contents):
+    "Iterates over all the matches, and will yield returned"
+    for item in pattern.finditer(file_contents):
+        "Yield, means: give back a single item, go further a new item is requested"
         yield item.replace("\n", "")
 
 
-def get_enum_definition(enum_string: str):
+def get_enum_definition(enum_string: str) -> CxxEnum:
+    "Converts a enum definition represented as a string to a CXX enum object"
+    "Get the name of the enum"
     name = re.findall("(?<=enum class )\w+", enum_string)[0]
+    "Get the defined type of the enum"
     inner_type = re.findall(":\s*(\w+)", enum_string)[0]
+    "Get all the items of that are defined inside the enum class"
     inner_items = re.findall("\{([^{}]+)\}", enum_string)[0].replace(" ", "").split(",")
     return CxxEnum(name, inner_type, inner_items)
 
 def get_github_file(repository: str, branch: str, file: str):
+    """Helper function to get a github file from a given repository"""
     return (
         urllib.request.urlopen(
             f"https://raw.githubusercontent.com/R2D2-2019/{repository}/{branch}/{file}")
             .read()
             .decode("utf-8")
     )
-def get_enum_definitions():
+def get_enum_definitions() -> list[CxxEnum]:
+    """Easy to call function to get all enums defined inside the frame_enums.hpp defined inside the external communications file """
+    "Get the file from GitHub"
     file_content = get_github_file("internal_communication", "master", "code/headers/frame_enums.hpp")
+    "Seperate file in seperate enum strings"
     enum_strings = get_enum_strings(file_content)
+    "Loop over all enum strings "
     for enum_string in enum_strings:
         yield get_enum_definition(enum_string)
 
-
-
-def convert_to_python(enum: CxxEnum):
-    enum
 
 
 if __name__ == "__main__":
--- tooling/enum_writer.py
+++ tooling/enum_writer.py
@@ -1,33 +1,50 @@
-from tooling.enum_parser import CxxEnum
-HEADER = """
 #! python
-\""" this generated file defines Frames
 
-it is based on the definitons of https://raw.githubusercontent.com/R2D2-2019/internal_communication/master/code/headers/frame_types.hpp
+from tooling.enum_parser import CxxEnum
+import datetime
 
-if you have a question or a problem.
-please make a github issue on https://github.com/R2D2-2019/r2d2-python-build/issues/new
-or look at https://github.com/R2D2-2019/r2d2-python-build#faq
-\"""
+def get_enum_file_header(maintainer: str, date: datetime.datetime, status: str = "Production"):
+    return f"""
+    #! python
 
-from common.common import AutoNumber
+    \""" this generated file defines Frames
 
-__maintainer__ = "Sebastiaan Saarloos"
-__date__ = "2019-06-04 17:09:01.153106"
-__status__ = "Production"
-"""
+    it is based on the definitons of https://raw.githubusercontent.com/R2D2-2019/internal_communication/master/code/headers/frame_types.hpp
+
+    if you have a question or a problem.
+    please make a github issue on https://github.com/R2D2-2019/r2d2-python-build/issues/new
+    or look at https://github.com/R2D2-2019/r2d2-python-build#faq
+    \"""
+
+    from common.common import AutoNumber
+
+    __maintainer__ = "{maintainer}"
+    __date__ = "{date}"
+    __status__ = "{status}"
+    """
+
+
+
 
 
 def convert_enum_to_python(cxx_enum_object: CxxEnum) -> str:
+    """Create a string representation of thje CxxEnum object in Python"""
+    "Create class signature "
     text = F"class {cxx_enum_object.name}(AutoNumber): \n"
+    "Add all items to the classs definition"
     for item in cxx_enum_object:
         text += f"    {item} = ()\n"
+    "Return the string"
     return text
 
 def convert_enums_to_fileformat(enum_text_list: list):
-    file = HEADER + '\n'
+    """Create the fileformat for the enum file"""
+    "Generate a file header"
+    file = get_enum_file_header("Sebastiaan Saarloos", datetime.datetime.now(), "Production") + '\n'
+    "Iterate over all enum strings"
     for enum_text in enum_text_list:
         file += enum_text + '\n'
+    "Give back the file contents"
     return file
 
 
